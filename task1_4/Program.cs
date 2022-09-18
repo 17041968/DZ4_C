ec24719d91d8c296792a76dc8e10a6e8720ba806
Yuri <12345@mail.ru>
2022-09-19 03:43:36 +0400

    task1_4

---
+++ task1_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B. Не использовать функцию Math.Pow()
+
+int Exponentiation(int numA, int numB)
+{
+  int result = 1;
+  for(int i = 1; i <= numB; i++)
+  {
+    result = result * numA;
+  }
+    return result;
+}
+
+  Console.Write("Ввести число A: ");
+  int numA = Convert.ToInt32(Console.ReadLine());
+
+  Console.Write("Ввести число B: ");
+  int numB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numA, numB);
+  Console.WriteLine("Число А в степени В: " + exponentiation);
+
